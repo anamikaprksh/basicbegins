dcab8fbe1b693b8ae1b8c10200d4945e640d4b6a
AbhishekS-2099 <abhi.sankar1997@gmail.com>
2019-05-22 23:55:40 +0530

    Added to gulp sass

--- BootStrap/gulpfile.js
+++ BootStrap/gulpfile.js
@@ -2,5 +2,16 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require(gulp-sass);
 
+gulp.task('serve' ['sass'], function(){
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','src/scss/*.scss'],['sass']);
+    gulp.watch("src/*.html").on('change',browserSync.reload);
+});
+
+
+
 
 gulp.task('default', ['js','serve']);
